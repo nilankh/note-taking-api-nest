b9eac1e5b25609d68180c284ab2d74555c9a6a88
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-05 13:08:59 +0530

    Connected to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesController } from './notes/notes.controller';
 import { NotesService } from './notes/notes.service';
+import { NotesModule } from './notes/notes.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config/keys'
 
 @Module({
-  imports: [],
+  imports: [NotesModule, MongooseModule.forRoot(config.mongoURI)],
   controllers: [AppController, NotesController],
   providers: [AppService, NotesService],
 })
---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://@note-taking-api.8xqoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
