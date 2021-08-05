362bad8a9dfcd177b9985841a4dc2e8e2c49e6fa
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-05 12:01:06 +0530

    learn about how to to use req.body in nest js

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CreateNoteDto } from './dto/create-note.dto';
+import { Request, Response } from 'express';
 
 @Controller('notes')
 export class NotesController {
-    @Get()
-    findAll():string{
-        return 'Get all items'
-    }
+  @Get()
+  findAll(@Req() req: Request, @Res() res: Response): Response {
+    console.log(req.url);
+    return res.send('Hello world');
+  }
 
-    @Post()
-    create(@Body() createNoteDto: CreateNoteDto): string{
-        return `Titile: ${createNoteDto.title} Content: ${createNoteDto.content}`;
-    }
+  @Post()
+  create(@Body() createNoteDto: CreateNoteDto): string {
+    return `Title: ${createNoteDto.title} Content: ${createNoteDto.content}`;
+  }
 }
